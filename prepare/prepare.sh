fb55214d01a73793c6ff45123b496b8cfb4a6e4b
Takuya Niita <takuya2230@gmail.com>
2022-04-04 16:18:21 +0900

    スクリプト修正

--- prepare/prepare.sh
+++ prepare/prepare.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 #Get CompartmentID
 oci iam compartment list | grep compartment-id > compartment-id.txt ; cut -f 2 -d ":" compartment-id.txt | tr -d ' ','"',',' | tee compartment-id-tee.txt &>/dev/null ; compartment_id=`cat ./compartment-id-tee.txt` ; rm -rf ./compartment-id* ; echo ${compartment_id}
-#Create Dynamic-Group
-oci iam dynamic-group create --name OCI_DevOps_Dynamic_Group --description OCI_DevOps_Dynamic_Group --matching-rule "Any {All {resource.type = 'devopsrepository', resource.compartment.id = '${compartment_id}'},All {resource.type = 'devopsbuildpipeline', resource.compartment.id = '${compartment_id}'},All {resource.type = 'devopsdeploypipeline', resource.compartment.id = '${compartment_id}'}}"
 #Create Policy
-oci iam policy create --name OCI_DevOps_Policy --description OCI_DevOps_Policy --compartment-id "${compartment_id}" --statements '["Allow dynamic-group OCI_DevOps_Dynamic_Group to manage devops-family in compartment id '${compartment_id}'","Allow dynamic-group OCI_DevOps_Dynamic_Group to manage all-artifacts in compartment id '${compartment_id}'","Allow dynamic-group OCI_DevOps_Dynamic_Group to manage cluster-family in compartment id '${compartment_id}'","Allow dynamic-group OCI_DevOps_Dynamic_Group to use ons-topics in compartment id '${compartment_id}'"]'
+oci iam policy create --name OCI_DevOps_Policy_OKE --description OCI_DevOps_Policy_OKE --compartment-id "${compartment_id}" --statements '["Allow dynamic-group OCI_DevOps_Dynamic_Group to manage cluster-family in compartment id '${compartment_id}'"]'
